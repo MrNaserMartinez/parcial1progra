79b86826d8a88ed072772e749564c1f0639e4e4b
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-28 22:40:13 -0600

    si

--- parcial1progra/Program.cs
+++ parcial1progra/Program.cs
@@ -1,11 +1,12 @@
-﻿Console.WriteLine("BIEVENIDO AL SISTEMA NUMERO 1 PARA EL PARCIAL 1");
+﻿
+Console.WriteLine("BIENVENIDO AL SISTEMA NUMERO 1 PARA EL PARCIAL 1");
 int opcion;
 
 do
 {
     Console.WriteLine("Menú:");
     Console.WriteLine("1. Calcular el factorial del número");
-    Console.WriteLine("2. Calcular la raíz cuadrada de un numero");
+    Console.WriteLine("2. Calcular la raíz cuadrada de un número");
     Console.WriteLine("3. Salir");
     Console.Write("Elige una opción: ");
 
@@ -20,7 +21,12 @@ do
     switch (opcion)
     {
         case 1:
-            Console.WriteLine("Has elegido la Opción 1.");
+            Console.WriteLine("Listo para calcular el factorial.");
+            Console.Write("Ingresa un número para calcular su factorial: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            long factorial = CalcularFactorial(numero);
+            Console.WriteLine($"El factorial de {numero} es: {factorial}");
             break;
         case 2:
             Console.WriteLine("Has elegido la Opción 2.");
@@ -33,4 +39,20 @@ do
             break;
     }
 } while (opcion != 3);
-    
+
+    static long CalcularFactorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            long resultado = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
+    }
