30b2be4a17e2dd7be56a35e9cfad4402374583a3
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-29 18:58:44 -0600

    final

--- parcial1progra/Program.cs
+++ parcial1progra/Program.cs
@@ -2,16 +2,40 @@
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 Console.WriteLine("BIENVENIDO AL SISTEMA NUMERO 1 PARA EL PARCIAL 1\n\n");
-Console.WriteLine("EJERCICIO #3 PARA EL PARCIAL 1\n");
-
-Console.WriteLine("TU TABLA DE MULTIPLICAR");
-Console.WriteLine("para esto solo debes de colocar el numero que deseas");
-Console.WriteLine("Ingresa el numero que deseas ver en tabla de 10");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("EJERCICIO #4 PARA EL PARCIAL 1\n");
 
 
-for (int i = 1; i <= 10; i++)
+Console.WriteLine("ADIVINA EL NUMERO SECRETO ENTRE 1 Y 100\n");
+Console.WriteLine("Bienvenido al juego de adivinar el número secreto!");
+Console.WriteLine("El número secreto está entre 1 y 100.");
+
+int elegido = 33; 
+
+int intentos = 0;
+int noelegido;
+
+do
 {
-    int resultado = n * i;
-    Console.WriteLine($"{n} x {i} = {resultado}");
-}
+    Console.Write("Introduce tu intento: ");
+    if (!int.TryParse(Console.ReadLine(), out noelegido))
+    {
+        Console.WriteLine("Error: Debes ingresar un número entero.");
+        continue;
+    }
+    intentos++;
+
+    if (noelegido < elegido)
+    {
+        Console.WriteLine("El número secreto es mayor.");
+    }
+    else if (noelegido > elegido)
+    {
+        Console.WriteLine("El número secreto es menor.");
+    }
+    else
+    {
+        Console.WriteLine($"ESOOOOOO {elegido} COMO EL GRAN FERNANDO ALONSO");
+        Console.WriteLine($"Solo te hiciste {intentos} intentos. XD");
+    }
+
+} while (noelegido != elegido);
